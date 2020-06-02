626745f582493ef757912fe03420bdbd25612aaa
soumenh <61101884+soumenh@users.noreply.github.com>
2020-06-02 22:49:53 +0530

    Add files via upload

---
+++ at.sh
@@ -0,0 +1,57 @@
+clear
+echo -e "\e[34m [Select Your Pubg] "
+PS3='
+Enter Your choice ~> '
+echo -e "\e[33m "
+
+options=("GL"
+"KR"
+"Quit")
+select opt in "${options[@]}"
+do
+case $opt in
+"GL")
+PS3='
+Enter Your choice ~> '
+echo -e "\e[33m "
+options=("       ON"
+"       OF"
+"       BACK")
+select opt in "${options[@]}"
+do
+case $opt in
+"       ON")
+echo "hack on"
+exit 0
+;;
+"       OF")
+echo "hack off"
+exit 0
+;;
+"       BACK")
+bash t.sh
+
+
+;;
+*) echo "invalid option $REPLY" && sleep 1  ;;
+esac
+done
+;;
+"KR")
+echo "searching update..."
+if ls /sdcard/t.sh ; then
+echo " YOU ARE ALREADY UPDATED"
+else
+echo "update found, please update"
+exit 0
+fi
+;;
+"Quit")
+exit 0
+
+;;
+*) echo "invalid option $REPLY" && sleep 1  ;;
+esac
+done
+
+
