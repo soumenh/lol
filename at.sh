d2af29756040e45daf692d1b7cb04537c58837fb
soumenh <61101884+soumenh@users.noreply.github.com>
2020-06-02 23:10:21 +0530

    Update at.sh

--- at.sh
+++ at.sh
@@ -1,6 +1,6 @@
 clear
-rm -rf lol &> dev/null
-git clone https://github.com/soumenh/lol &> /dev/null
+rm -rf lol 
+git clone https://github.com/soumenh/lol
 echo -e "\e[34m [Select Your Pubg] "
 PS3='
 Enter Your choice ~> '
